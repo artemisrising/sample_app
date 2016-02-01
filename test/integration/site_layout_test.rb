2d22f1e243ea2533144e482917e708c3346aae26
ARTEMISRISING <artemisrising@users.noreply.github.com>
2016-02-01 01:52:36 -0800

    Worked on tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,6 +9,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
   # test "the truth" do
   #   assert true
